bbdd11c8af63f6b6a9e9ef044e72335937f1dba3
Shu W <shuwang127@yahoo.com>
2020-03-13 16:51:47 -0400

    Add files via upload

--- disaster_detection.py
+++ disaster_detection.py
@@ -17,6 +17,7 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
 from nltk.util import ngrams
 from nltk import word_tokenize
+from sklearn.preprocessing import OneHotEncoder
 from itertools import chain
 from collections import defaultdict
 from sklearn.model_selection import train_test_split
@@ -49,97 +50,24 @@ def demo(dataTrain, dataTest, dList, typeStem, typeFeat, method):
     if method not in ['NaiveBayes', 'Logistic']:
         print('[Error] Classifier setting invalid!')
         return
-    print('[Demo] Data: %s | Feature: %s | Classifier: %s.' % (typeStem, typeFeat, method))
+    print('[Demo] ------ Data: %s | Feature: %s | Classifier: %s ------' % (typeStem, typeFeat, method))
     # extract training features and labels.
     featTrain, labelTrain = ExtractFeatures(dataTrain, dList, 'Train', typeStem, typeFeat)
-    print('[Info] Get %s training features and labels.' % (typeFeat))
+    print('[Info] Get %d \'%s\' training features (dim:%d) and labels (dim:1).' % (len(featTrain), typeFeat, len(featTrain[0])))
     # train the model.
-    # model = NaiveBayesTrain(featTrain, labelTrain)
+    if 'NaiveBayes' == method:
+        prior, likelihood = NaiveBayesTrain(featTrain, labelTrain)
+    print('[Info] %s classifier training done!' % (method))
     # extract testing features and labels.
-    ExtractFeatures(dataTest, dList, 'Test', typeStem, typeFeat)
-    print('[Info] Get %s testing features and labels.' % (typeFeat))
+    featTest, labelTest = ExtractFeatures(dataTest, dList, 'Test', typeStem, typeFeat)
+    print('[Info] Get %d \'%s\' testing features (dim:%d) and labels (dim:1).' % (len(featTest), typeFeat, len(featTest[0])))
     # test the model.
-    # predTest = NaiveBayesTest(model, featTest)
+    #if 'NaiveBayes' == method:
+    #    predTest = NaiveBayesTest(prior, likelihood, featTest)
     # evaluate.
     # accuracy, confusion = OutputEval(predTest, labelTest)
     # test the data
     return
-
-def ExtractFeatures(dataset, dList, typeSet, typeStem, typeFeat):
-    # input validation.
-    if typeSet not in ['Train', 'Test']:
-        print('[Error] Dataset setting invalid!')
-        return
-    # sparse the corresponding dataset.
-    data = dList['list' + typeSet + typeStem]
-    D = len(data)
-    # build the vocabulary from training set.
-    vocab = list(set(list(chain.from_iterable(dList['listTrain' + typeStem]))))
-    V = len(vocab)
-    vocabDict = dict(zip(vocab, range(V)))
-    print('[Info] Load %d %s vocabulary words.' % (V, typeStem))
-
-    label = list(dataset['target'])
-    features = np.zeros((D, V))
-
-    # get the feature matrix (Frequency).
-    if 'Frequency' == typeFeat:
-        for ind, doc in enumerate(data):
-            for item in doc:
-                if item in vocabDict:
-                    features[ind][vocabDict[item]] += 1
-    # get the feature matrix (Binary).
-    if 'Binary' == typeFeat:
-        for ind, doc in enumerate(data):
-            for item in doc:
-                if item in vocabDict:
-                    features[ind][vocabDict[item]] = 1
-    # get the feature matrix (TFIDF):
-    if 'TFIDF' == typeFeat:
-        if os.path.exists(tempPath + '/tfidf_' + typeSet + typeStem + '.npy'):
-            features = np.load(tempPath + '/tfidf_' + typeSet + typeStem + '.npy')
-        else:
-            # get freq and bin features.
-            termFreq = np.zeros((D, V))
-            termBin = np.zeros((D, V))
-            for ind, doc in enumerate(data):
-                for item in doc:
-                    if item in vocabDict:
-                        termFreq[ind][vocabDict[item]] += 1
-                        termBin[ind][vocabDict[item]] = 1
-            # get tf (1+log10)
-            tf = np.zeros((D, V))
-            for ind in range(D):
-                for i in range(V):
-                    if termFreq[ind][i] > 0:
-                        tf[ind][i] = 1 + math.log(termFreq[ind][i], 10)
-            del termFreq
-            # find idf
-            if os.path.exists(tempPath + '/idf_' + typeStem + '.npy'):
-                idf = np.load(tempPath + '/idf_' + typeStem + '.npy')
-            elif 'Train' == typeSet:
-                # get df
-                df = np.zeros((V, 1))
-                for ind in range(D):
-                    for i in range(V):
-                        df[i] += termBin[ind][i]
-                # get idf (log10(D/df))
-                idf = np.zeros((V, 1))
-                for i in range(V):
-                    if df[i] > 0:
-                        idf[i] = math.log(D, 10) - math.log(df[i], 10)
-                del df
-                np.save(tempPath + '/idf_' + typeStem + '.npy', idf)
-            else:
-                print('[Error] Need file: %s/idf_%s.npy to process test data!' % (tempPath, typeStem))
-                return
-            del termBin
-            # get tfidf
-            for ind in range(D):
-                for i in range(V):
-                    features[ind][i] = tf[ind][i] * idf[i]
-            np.save(tempPath + '/tfidf_' + typeSet + typeStem + '.npy', features)
-    return features, label
 
 def ReadCsvData():
     # validate temp path
@@ -266,6 +194,137 @@ def CreateVocabulary(dataTrain, dataTest):
     np.savez(tempPath + 'list.npz', listTrainNoStem=listTrainNoStem, listTrainStem=listTrainStem, listTestNoStem=listTestNoStem, listTestStem=listTestStem)
     print('[Info] Load text list (noStem/Stem) of train/test set from %s/list.npz.' % (tempPath))
     return np.load(tempPath + 'list.npz', allow_pickle = True)
+
+def ExtractFeatures(dataset, dList, typeSet, typeStem, typeFeat):
+    # input validation.
+    if typeSet not in ['Train', 'Test']:
+        print('[Error] Dataset setting invalid!')
+        return
+    # sparse the corresponding dataset.
+    data = dList['list' + typeSet + typeStem]
+    D = len(data)
+    # build the vocabulary from training set.
+    vocab = list(set(list(chain.from_iterable(dList['listTrain' + typeStem]))))
+    V = len(vocab)
+    vocabDict = dict(zip(vocab, range(V)))
+    print('[Info] Load %d \'%s\' vocabulary words.' % (V, typeStem))
+
+    # get labels.
+    labels = np.array(dataset['target']).reshape(-1, 1)
+    # get text features.
+    features = np.zeros((D, V))
+    # get the feature matrix (Frequency).
+    if 'Frequency' == typeFeat:
+        for ind, doc in enumerate(data):
+            for item in doc:
+                if item in vocabDict:
+                    features[ind][vocabDict[item]] += 1
+    # get the feature matrix (Binary).
+    if 'Binary' == typeFeat:
+        for ind, doc in enumerate(data):
+            for item in doc:
+                if item in vocabDict:
+                    features[ind][vocabDict[item]] = 1
+    # get the feature matrix (TFIDF):
+    if 'TFIDF' == typeFeat:
+        if os.path.exists(tempPath + '/tfidf_' + typeSet + typeStem + '.npy'):
+            features = np.load(tempPath + '/tfidf_' + typeSet + typeStem + '.npy')
+        else:
+            # get freq and bin features.
+            termFreq = np.zeros((D, V))
+            termBin = np.zeros((D, V))
+            for ind, doc in enumerate(data):
+                for item in doc:
+                    if item in vocabDict:
+                        termFreq[ind][vocabDict[item]] += 1
+                        termBin[ind][vocabDict[item]] = 1
+            # get tf (1+log10)
+            tf = np.zeros((D, V))
+            for ind in range(D):
+                for i in range(V):
+                    if termFreq[ind][i] > 0:
+                        tf[ind][i] = 1 + math.log(termFreq[ind][i], 10)
+            del termFreq
+            # find idf
+            if os.path.exists(tempPath + '/idf_' + typeStem + '.npy'):
+                idf = np.load(tempPath + '/idf_' + typeStem + '.npy')
+            elif 'Train' == typeSet:
+                # get df
+                df = np.zeros((V, 1))
+                for ind in range(D):
+                    for i in range(V):
+                        df[i] += termBin[ind][i]
+                # get idf (log10(D/df))
+                idf = np.zeros((V, 1))
+                for i in range(V):
+                    if df[i] > 0:
+                        idf[i] = math.log(D, 10) - math.log(df[i], 10)
+                del df
+                np.save(tempPath + '/idf_' + typeStem + '.npy', idf)
+            else:
+                print('[Error] Need file: %s/idf_%s.npy to process test data!' % (tempPath, typeStem))
+                return
+            del termBin
+            # get tfidf
+            for ind in range(D):
+                for i in range(V):
+                    features[ind][i] = tf[ind][i] * idf[i]
+            np.save(tempPath + '/tfidf_' + typeSet + typeStem + '.npy', features)
+
+    # mix features.
+    # loc? (1 or 0)
+    featloc = np.array(dataset['loc']).reshape(-1, 1)
+    features = np.hstack((features, featloc))
+    # url number.
+    featurl = np.array(dataset['url']).reshape(-1, 1)
+    features = np.hstack((features, featurl))
+    # keywd onehot (222).
+    featkeywd = np.array(dataset['keywd']).reshape(-1, 1)
+    onehot_encoder = OneHotEncoder(sparse = False)
+    featkeywd = onehot_encoder.fit_transform(featkeywd)
+    features = np.hstack((features, featkeywd))
+    # return features and labels
+    return features, labels
+
+def NaiveBayesTrain(features, labels):
+    # define the log prior.
+    def GetLogPrior(labelTrain):
+        # count the number.
+        nDoc = len(labelTrain)
+        nPos = list(labelTrain).count(1)
+        nNag = list(labelTrain).count(0)
+        # calculate the logprior.
+        priorPos = math.log(nPos / nDoc)
+        priorNag = math.log(nNag / nDoc)
+        prior = [priorNag, priorPos]
+        return prior
+
+    # define loglikelihood.
+    def GetLogLikelihood(features, labels):
+        # get V and D.
+        V = len(features[0])
+        D = len(features)
+        cls = 2
+        # initilaze likelihood matrix.
+        likelihood = np.zeros((cls, V))
+        for ind in range(D):
+            for i in range(V):
+                likelihood[labels[ind][0]][i] += features[ind][i]
+        # Laplace smoothing.
+        denom = np.zeros((cls, 1))
+        for lb in range(cls):
+            denom[lb] = sum(likelihood[lb]) + V
+            for i in range(V):
+                likelihood[lb][i] += 1
+                likelihood[lb][i] /= denom[lb]
+                likelihood[lb][i] = math.log(likelihood[lb][i])
+        return likelihood
+
+    # get the log prior.
+    prior = GetLogPrior(labels)
+    # get the log likelihood
+    likelihood = GetLogLikelihood(features, labels)
+    return prior, likelihood
 
 if __name__ == "__main__":
     main()
